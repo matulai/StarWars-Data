95e81d50938ade32839cd533686b72eddcb870c4
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-04-17 11:10:03 +0000

    supercool navbar

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,22 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import StarWarsLogo from "../../img/Star_Wars_logo.png"
-import "../../styles/navbar.css";
+import "../../styles/nav.scss";
 
 export const Navbar = () => {
+	const [isNavVisible, setNavVisible] = useState(false);
+
+	const toggleNav = () => {
+	  setNavVisible(!isNavVisible);
+	};
+  
 	return (
-		<div className="container-fluid">
-			<div className="row">
-				<nav className="navbar navbar-bd m-3 d-flex justify-content-center">
-					<div className="col-sm-12 col-md-10 col-lg-8">
-						<Link to="/">
-							<img className="sw-logo" src={StarWarsLogo}></img>
-						</Link>
-						<div className="ml-auto">
-						</div>
-					</div>
-				</nav>
+		<div>
+			<div id="container">
+				<div
+					id="burger"
+					className={isNavVisible ? "active" : ""}
+					onClick={toggleNav}
+				>
+					<div className="bun top"></div>
+					<div className="filling"></div>
+					<div className="bun bottom"></div>
+				</div>
 			</div>
+	
+			<nav className={isNavVisible ? "show" : ""}>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li className="red">
+						<Link to="/characters">Characters</Link>
+					</li>
+					<li className="orange">
+						<Link to="/films">Films</Link>
+					</li>
+					<li className="green">
+						<Link to="/species">Species</Link>
+					</li>
+					<li className="yellow">
+						<Link to="/starships">Starships</Link>
+					</li>
+					<li className="blue">
+						<Link to="/vehicles">Vehicles</Link>
+					</li>
+					<li className="purple">
+						<Link to="/planets">Planets</Link>
+					</li>
+				</ul>
+			</nav>
 		</div>
 	);
 };
