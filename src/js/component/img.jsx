6b7f723e8a7704f3e4b5b5581e2974315b13b5f6
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-04-18 16:02:38 +0000

    images now work properly

---
+++ src/js/component/img.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import images from "../images";
+
+const MyComponent = ({ category, uid }) => {
+  const prefix =
+    category === "starships"
+      ? "st"
+      : category === "species"
+      ? "sp"
+      : category[0];
+
+  const imageName = `${prefix}${uid}`;
+
+  return <img src={images[imageName]} alt={`Image ${uid}`} />;
+};
+
+export default MyComponent;
