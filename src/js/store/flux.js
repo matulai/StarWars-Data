18108ff2c4e8d7b64f7e4e5c682871930d6c93e6
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-04-18 16:04:06 +0000

    succesfully fetches the first page of each category

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,50 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+  return {
+    store: {
+      currentPage: {
+        characters: 1,
+        films: 1,
+        species: 1,
+        starships: 1,
+        vehicles: 1,
+        planets: 1,
+      },
+      charactersList: {},
+      filmsList: {},
+      speciesList: {},
+      starshipsList: {},
+      vehiclesList: {},
+      planetsList: {},
+    },
+    actions: {
+      getData: (category, direction = 0) => {
+        let people = null;
+        if (category === "characters") people = "people";
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+        const store = getStore();
+        const page = store.currentPage[category] + direction;
+        fetch(
+          `https://www.swapi.tech/api/${
+            people || category
+          }?page=${page}&limit=10`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            const updatedCurrentPage = {
+              ...store.currentPage,
+              [category]: page,
+            };
+            setStore({ currentPage: updatedCurrentPage });
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+            const info = category === "films" ? data.result : data.results;
+
+            const updatedList = { ...store[`${category}List`], [page]: info };
+            setStore({ [`${category}List`]: updatedList });
+          })
+          .catch((error) => console.error(error));
+      },
+    },
+  };
 };
 
 export default getState;
