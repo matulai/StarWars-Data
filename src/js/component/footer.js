bd38e902e8e13122689b66929c78e93acc5427a0
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-04-19 15:23:34 +0000

    deleted unused stuff

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 
 export const Footer = () => (
-  <footer className="footer mt-auto py-3 text-center text-white fixed-bottom">
+  <footer className="footer mt-auto py-3 text-center text-white">
     <p>
       Made with <i className="fa fa-heart text-danger" /> by Blondy Martínez
     </p>
