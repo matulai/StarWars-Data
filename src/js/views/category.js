617abf24e0ad5735d35215bd21c9aae11eabf6c5
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-04-20 21:13:22 +0000

    prev and next page functional  buttons

--- src/js/views/category.js
+++ src/js/views/category.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
 import CategoryComp from "../component/category.jsx";
 
 import "../../styles/category.css";
 
 export const Category = () => {
   const { category } = useParams();
+  const { store, actions } = useContext(Context);
 
-  return <CategoryComp category={category}></CategoryComp>;
+  return (
+    <div>
+      <div>
+        <button
+          onClick={() => {
+            actions.getData(category, 1);
+          }}
+        >
+          Next
+        </button>
+        <button
+          onClick={() => {
+            actions.getData(category, -1);
+          }}
+        >
+          Prev
+        </button>
+      </div>
+      <CategoryComp category={category}></CategoryComp>
+    </div>
+  );
 };
