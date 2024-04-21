18c43eaf27753892ea241ddc3d66bb9b21c9ba46
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-04-21 12:34:05 +0000

    switched icon for unicode

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -2,8 +2,6 @@ import React from "react";
 
 export const Footer = () => (
   <footer className="footer mt-auto py-3 text-center text-white">
-    <p>
-      Made with <i className="fa fa-heart text-danger" /> by Blondy Martínez
-    </p>
+    <p>Made with ❤️ by Blondy Martínez</p>
   </footer>
 );
