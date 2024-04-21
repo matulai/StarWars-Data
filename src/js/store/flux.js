872a4491cc5a57d67470a55744ed28df61a7cf42
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-04-21 00:29:47 +0000

    added page validation and tidied up

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -9,6 +9,8 @@ const getState = ({ getStore, getActions, setStore }) => {
         vehicles: 1,
         planets: 1,
       },
+
+      totalPages: {},
       charactersList: {},
       filmsList: {},
       speciesList: {},
@@ -16,13 +18,34 @@ const getState = ({ getStore, getActions, setStore }) => {
       vehiclesList: {},
       planetsList: {},
     },
+
     actions: {
-      getData: (category, direction = 0) => {
+      getData: (category, direction = 0, toPage = false) => {
         let people = null;
         if (category === "characters") people = "people";
 
         const store = getStore();
-        const page = store.currentPage[category] + direction;
+        const page = toPage
+          ? direction
+          : getActions().validPage(
+              store.currentPage[category] + direction,
+              category
+            );
+
+        console.log(
+          page,
+          store.totalPages[category],
+          getActions().validPage(
+            store.currentPage[category] + direction,
+            category
+          )
+        );
+        if (
+          page === store.currentPage[category] &&
+          Object.keys(store[`${category}List`]).length !== 0
+        )
+          return;
+
         fetch(
           `https://www.swapi.tech/api/${
             people || category
@@ -30,18 +53,39 @@ const getState = ({ getStore, getActions, setStore }) => {
         )
           .then((response) => response.json())
           .then((data) => {
-            const updatedCurrentPage = {
-              ...store.currentPage,
-              [category]: page,
-            };
-            setStore({ currentPage: updatedCurrentPage });
-
-            const info = category === "films" ? data.result : data.results;
-
-            const updatedList = { ...store[`${category}List`], [page]: info };
-            setStore({ [`${category}List`]: updatedList });
+            getActions().setStoreData(category, page, data);
           })
           .catch((error) => console.error(error));
+      },
+
+      validPage: (page, category) => {
+        if (page < 1) return 1;
+        if (page > parseInt(getStore().totalPages[category]))
+          return parseInt(getStore().totalPages[category]);
+        return page;
+      },
+
+      setStoreData: (category, page, data) => {
+        const store = getStore();
+
+        const updatedCurrentPage = {
+          ...store.currentPage,
+          [category]: page,
+        };
+        setStore({ currentPage: updatedCurrentPage });
+
+        if (!store.totalPages[category]) {
+          const updatedTotalPages = {
+            ...store.totalPages,
+            [category]: data.total_pages,
+          };
+          setStore({ totalPages: updatedTotalPages });
+        }
+
+        const info = category === "films" ? data.result : data.results;
+
+        const updatedList = { ...store[`${category}List`], [page]: info };
+        setStore({ [`${category}List`]: updatedList });
       },
     },
   };
