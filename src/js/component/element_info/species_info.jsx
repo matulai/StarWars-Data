bcafa8d165f8374fff2ff5853977f0eb700e8b01
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-04-22 02:27:27 +0000

    added species, starship and vehicle individual info components

---
+++ src/js/component/element_info/species_info.jsx
@@ -0,0 +1,94 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../../store/appContext";
+import { Link } from "react-router-dom";
+import Img from "../img.jsx";
+
+const SpeciesInfo = (props) => {
+  const { actions } = useContext(Context);
+  const [info, setInfo] = useState({});
+  const [homeworld, setHomeworld] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await actions.getSingleData(props.category, props.uid);
+      setInfo(data.properties);
+
+      const homeworldArr = data.properties.homeworld.split("/");
+      const homeworldData = await actions.getSingleData(
+        "planets",
+        homeworldArr[homeworldArr.length - 1]
+      );
+      setHomeworld(homeworldData);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="card w-100">
+      <div className="row d-flex p-2">
+        <div className="col-auto">
+          <Img category={props.category} uid={props.uid}></Img>
+        </div>
+        <div className="col d-flex flex-column">
+          <div className="text-center">
+            <h1>{info.name}</h1>
+            <hr />
+          </div>
+          <ul>
+            <li>
+              <span>
+                <b>Classification:</b> {info.classification}
+              </span>
+            </li>
+            <li>
+              <span>
+                <b>Designation:</b> {info.designation}
+              </span>
+            </li>
+            <li>
+              <span>
+                <b>Hair colors:</b> {info.hair_colors}
+              </span>
+            </li>
+            <li>
+              <span>
+                <b>Eye colors:</b> {info.eye_colors}
+              </span>
+            </li>
+            <li>
+              <span>
+                <b>Skin colors:</b> {info.skin_colors}
+              </span>
+            </li>
+            <li>
+              <span>
+                <b>Average Height:</b> {info.average_height}cm
+              </span>
+            </li>
+            <li>
+              <span>
+                <b>Average Lifespan:</b> {info.average_lifespan} years
+              </span>
+            </li>
+            <li>
+              <span>
+                <b>Homeworld:</b>{" "}
+                <Link to={`/planets/${homeworld?.uid}`}>
+                  {homeworld?.properties?.name}
+                </Link>
+              </span>
+            </li>
+            <li>
+              <span>
+                <b>Language:</b> {info.language}
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SpeciesInfo;
---
+++ src/js/component/element_info/vehicle_info.jsx
@@ -0,0 +1,88 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../../store/appContext";
+import Img from "../img.jsx";
+
+const VehicleInfo = (props) => {
+  const { actions } = useContext(Context);
+  const [info, setInfo] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await actions.getSingleData(props.category, props.uid);
+      setInfo(data.properties);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="card w-100">
+      <div className="row d-flex p-2">
+        <div className="col-auto">
+          <Img category={props.category} uid={props.uid}></Img>
+        </div>
+        <div className="col d-flex flex-column">
+          <div className="text-center">
+            <h1>{info.name}</h1>
+            <hr />
+          </div>
+          <ul>
+            <li>
+              <span>
+                <b>Model:</b> {info.model}
+              </span>
+            </li>
+            <li>
+              <span>
+                <b>Vehicle Class:</b> {info.vehicle_class}
+              </span>
+            </li>
+            <li>
+              <span>
+                <b>Manufacturer:</b> {info.manufacturer}
+              </span>
+            </li>
+            <li>
+              <span>
+                <b>Cost:</b> {info.cost_in_credits} credits
+              </span>
+            </li>
+            <li>
+              <span>
+                <b>Crew:</b> {info.crew}
+              </span>
+            </li>
+            <li>
+              <span>
+                <b>Length:</b> {info.length}m
+              </span>
+            </li>
+            <li>
+              <span>
+                <b>Passengers:</b> {info.passengers}
+              </span>
+            </li>
+            <li>
+              <span>
+                <b>Max Speed:</b> {info.max_atmosphering_speed}{" "}
+                {info.max_atmosphering_speed !== "n/a" ? "km/h" : ""}
+              </span>
+            </li>
+            <li>
+              <span>
+                <b>Cargo Capacity:</b> {info.cargo_capacity}t
+              </span>
+            </li>
+            <li>
+              <span>
+                <b>Consumables:</b> {info.consumables}
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VehicleInfo;
