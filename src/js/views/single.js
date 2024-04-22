c444cd1ed8a0ea6135e32d858b13d8c134d7ca56
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-04-22 02:27:41 +0000

    show single component based on category and id

--- src/js/views/single.js
+++ src/js/views/single.js
@@ -3,6 +3,10 @@ import PropTypes from "prop-types";
 import { useParams } from "react-router-dom";
 import FilmInfo from "../component/element_info/film_info.jsx";
 import CharacterInfo from "../component/element_info/character_info.jsx";
+import PlanetInfo from "../component/element_info/planet_info.jsx";
+import SpeciesInfo from "../component/element_info/species_info.jsx";
+import StarshipInfo from "../component/element_info/starship_info.jsx";
+import VehicleInfo from "../component/element_info/vehicle_info.jsx";
 export const Single = () => {
   const { category, theid } = useParams();
 
@@ -14,6 +18,18 @@ export const Single = () => {
       break;
     case "characters":
       content = <CharacterInfo category={category} uid={theid} />;
+      break;
+    case "planets":
+      content = <PlanetInfo category={category} uid={theid} />;
+      break;
+    case "species":
+      content = <SpeciesInfo category={category} uid={theid} />;
+      break;
+    case "starships":
+      content = <StarshipInfo category={category} uid={theid} />;
+      break;
+    case "vehicles":
+      content = <VehicleInfo category={category} uid={theid} />;
       break;
   }
 
