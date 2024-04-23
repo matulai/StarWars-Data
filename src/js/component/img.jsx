1bc3cce9db0a390858094292e0b758fb07dcd539
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-04-23 01:50:16 +0000

    implemented lazy load to reduce initial load

--- src/js/component/img.jsx
+++ src/js/component/img.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import images from "../images";
+import React, { useState, useEffect } from "react";
 
 const Img = ({ category, uid }) => {
   const prefix =
@@ -8,12 +7,22 @@ const Img = ({ category, uid }) => {
       : category === "species"
       ? "sp"
       : category[0];
-
   const imageName = `${prefix}${uid}`;
 
-  return (
-    <img className="img-fluid" src={images[imageName]} alt={`Image ${uid}`} />
-  );
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    const loadImage = async () => {
+      const imageModule = await import(
+        `../../img/${category}/${imageName}.jpg`
+      );
+      setImage(imageModule.default);
+    };
+
+    loadImage();
+  }, [imageName]);
+
+  return <img className="img-fluid" src={image} alt={`Image ${uid}`} />;
 };
 
 export default Img;
